03d188adaf852e9e5321334b49fde5a34e51cbf8
CoolStar <coolstarorganization@gmail.com>
2022-03-03 23:15:35 -0800

    Print memory size. Fix API usage

--- Desc Race Test/Desc Race Test/ViewController.m
+++ Desc Race Test/Desc Race Test/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import <sys/sysctl.h>
 
 void desc_race(void);
 
@@ -17,6 +18,11 @@ void desc_race(void);
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    uint64_t mem;
+    size_t len = sizeof(uint64_t);
+    sysctlbyname("hw.memsize", &mem, &len, NULL, 0);
+    printf("Mem: %lld\n", mem);
     
     desc_race();
     // Do any additional setup after loading the view.
